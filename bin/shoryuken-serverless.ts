986f6b363173022b5db83fa437810553d2cde763
Pablo Cantero <pablohstc@gmail.com>
2018-11-30 17:56:39 -0800

    WIP lambda + sqs event source

--- bin/shoryuken-serverless.ts
+++ bin/shoryuken-serverless.ts
@@ -1,24 +1,34 @@
 #!/usr/bin/env node
-import sns = require('@aws-cdk/aws-sns');
-import sqs = require('@aws-cdk/aws-sqs');
-import cdk = require('@aws-cdk/cdk');
+import sns = require('@aws-cdk/aws-sns')
+import sqs = require('@aws-cdk/aws-sqs')
+import cdk = require('@aws-cdk/cdk')
+import lambda = require('@aws-cdk/aws-lambda')
+import { SqsEventSource } from '@aws-cdk/aws-lambda-event-sources'
 
 class ShoryukenServerlessStack extends cdk.Stack {
   constructor(parent: cdk.App, name: string, props?: cdk.StackProps) {
-    super(parent, name, props);
+    super(parent, name, props)
 
     const queue = new sqs.Queue(this, 'ShoryukenServerlessQueue', {
       visibilityTimeoutSec: 300
-    });
+    })
 
-    const topic = new sns.Topic(this, 'ShoryukenServerlessTopic');
+    const topic = new sns.Topic(this, 'ShoryukenServerlessTopic')
 
-    topic.subscribeQueue(queue);
+    topic.subscribeQueue(queue)
+
+    const fn = new lambda.Function(this, 'MyFunction', {
+      runtime: lambda.Runtime.NodeJS810,
+      handler: 'index.handler',
+      code: lambda.Code.asset('./lambda-handler')
+    })
+
+    fn.addEventSource(new SqsEventSource(queue))
   }
 }
 
-const app = new cdk.App();
+const app = new cdk.App()
 
-new ShoryukenServerlessStack(app, 'ShoryukenServerlessStack');
+new ShoryukenServerlessStack(app, 'ShoryukenServerlessStack')
 
-app.run();
+app.run()
