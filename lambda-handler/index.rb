615cff79d7e40a60f3aca104920c6d322805cd58
Pablo Cantero <pablohstc@gmail.com>
2018-12-15 09:05:27 -0500

    Parse message

--- lambda-handler/index.rb
+++ lambda-handler/index.rb
@@ -1,8 +1,38 @@
 require 'json'
+require 'ostruct'
+
+def to_message(record)
+  # sample record
+  # "messageId": "...",
+  # "receiptHandle": "...",
+  # "body": "...",
+  # "attributes": {
+  #     "ApproximateReceiveCount": "1",
+  #     "SentTimestamp": "1544881108683",
+  #     "SenderId": "...:pablo",
+  #     "ApproximateFirstReceiveTimestamp": "1544881108691"
+  # },
+  # "messageAttributes": {},
+  # "md5OfBody": "...",
+  # "eventSource": "aws:sqs",
+  # "eventSourceARN": "arn:aws:sqs:us-east-1::...",
+  # "awsRegion": "us-east-1"
+  #
+  # create a Shoryuken compatible message
+  # so that this https://github.com/phstc/shoryuken/blob/master/lib/shoryuken/default_worker_registry.rb#L15
+  # and this https://github.com/phstc/shoryuken/blob/352b20642b4b8a123b821228231ed5a5c618a9cc/lib/shoryuken/body_parser.rb
+  # work
+  OpenStruct.new(
+    message_attributes: record['messageAttributes'],
+    body: record['body'],
+    queue: record['eventSourceARN'].split(':').last
+  )
+end
 
 def handler(event:, context:)
-  result = { event: JSON.generate(event), context: JSON.generate(context.inspect) }
-  puts JSON.generate(event)
+  event['Records'].each do |record|
+    puts JSON.generate(to_message(record))
+  end
+
   puts JSON.generate(context.inspect)
-  result
 end
