1a0085a6c6e95a24c86db9260b9f81b3da8bad13
Pablo Cantero <pablohstc@gmail.com>
2018-12-15 08:20:41 -0500

    Update cdk; Try to prettify console output

--- lambda-handler/index.rb
+++ lambda-handler/index.rb
@@ -1,3 +1,8 @@
+require 'json'
+
 def handler(event:, context:)
-  puts "EVENT: #{event} \n\n CONTEXT: #{context}"
+  result = { event: JSON.generate(event), context: JSON.generate(context.inspect) }
+  puts JSON.generate(event)
+  puts JSON.generate(context.inspect)
+  result
 end
